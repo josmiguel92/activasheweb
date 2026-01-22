b42e62d55a9f5bc647d21c0196dd99a0198079b1
Josue M. Aguilar <josmiguel92@gmail.com>
2026-01-22 20:02:21 +0100

    improve SeoTags

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,14 @@ import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      entryLimit: 45000,
+    }),
+  ],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
